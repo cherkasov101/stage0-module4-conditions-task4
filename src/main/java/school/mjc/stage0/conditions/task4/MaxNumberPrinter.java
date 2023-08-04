8135d69556467d62c671ca13e1edddcbf39af86d
Iurii Cherkasov <cherkasov1010@gmail.com>
2023-08-04 15:42:46 +0300

    m4.1 t4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int biggest = first;
+        if (first < second || first < third) {
+            if (second > third) {
+                biggest = second;
+            } else {
+                biggest = third;
+            }
+        }
+        System.out.println(biggest);
     }
 }
